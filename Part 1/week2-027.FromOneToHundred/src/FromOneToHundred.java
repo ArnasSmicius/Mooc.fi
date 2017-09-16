a04031ad46076a2d4fc1ba064e02b8ec23ad6d5c
arnasmic@gmail.com <arnasmic@gmail.com>
2017-09-16 21:49:47 +0300

    FromOneToHundred

---
+++ Part 1/week2-027.FromOneToHundred/src/FromOneToHundred.java
@@ -0,0 +1,14 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+
+        int number;
+
+        for (number = 1 ; number <= 100; number++)
+        {
+            System.out.println(number);
+        }
+    }
+}
