6f21985d8b0f794a0adfceb03dfc7d9e7d32eacb
arnasmic@gmail.com <arnasmic@gmail.com>
2017-10-16 20:03:11 +0300

    TheGreatest

---
+++ Part 1/week3-066.TheGreatest/src/TheGreatest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class TheGreatest {
+    public static int greatest(ArrayList<Integer> list) {
+        int greatestDigit = Integer.MIN_VALUE;
+        for (int digit : list){
+            if (digit > greatestDigit){
+                greatestDigit = digit;
+            }
+        }
+        return greatestDigit;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        lista.add(3);
+        lista.add(2);
+        lista.add(7);
+        lista.add(2);
+        
+        System.out.println("The greatest number is: " + greatest(lista));
+    }
+}
