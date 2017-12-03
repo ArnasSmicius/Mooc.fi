4c30bd34764b256e83d99aedadffe9c41a094d27
arnasmic@gmail.com <arnasmic@gmail.com>
2017-12-03 20:01:47 +0200

    FirstPackages

---
+++ Part 2/week9-week9_17.FirstPackages/src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,11 @@
+package mooc.ui;
+
+/**
+ * Created by Arnas on 2017.03.02.
+ */
+public class TextUserInterface implements UserInterface {
+
+    public void update(){
+        System.out.println("Updating the user interface");
+    }
+}
---
+++ Part 2/week9-week9_17.FirstPackages/src/mooc/ui/UserInterface.java
@@ -0,0 +1,8 @@
+package mooc.ui;
+
+/**
+ * Created by Arnas on 2017.03.02.
+ */
+public interface UserInterface {
+    void update();
+}
